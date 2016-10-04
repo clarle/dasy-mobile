20ada6e5b1aff4fbc07679f3f0db917e99201721
Marius Craciunoiu <marius@craciunoiu.net>
2016-10-04 13:55:16 -0600

    Add react-native-image-picker

--- android/app/src/main/java/com/dasy/MainApplication.java
+++ android/app/src/main/java/com/dasy/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.kevinejohn.RNMixpanel.*;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImagePickerPackage(),
             new ReactNativeConfigPackage(),
             new VectorIconsPackage(),
             new RNMixpanel()
